9c30b321970796f4922394c7b01e48f18a831e79
dhruv <dhruvrajput1904200@gmail.com>
2024-12-24 22:53:29 +0530

    header compleate

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,11 @@
 let toggleButton = document.querySelector('.switch');
 let body = document.body;
 let header = document.querySelector('header');
+let seachInput = document.querySelector('.search');
 
 toggleButton.addEventListener('click', function() {
     toggleButton.classList.toggle('active');
     body.classList.toggle('dark-mode');
-    header.classList.toggle('dark-mode')
+    header.classList.toggle('dark-mode');
+    seachInput.classList.toggle('dark-mode');
 });
