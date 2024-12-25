68167015855364cf0c34584b21e1888525a1153e
dhruv <dhruvrajput1904200@gmail.com>
2024-12-25 22:42:34 +0530

    changes

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,14 @@ let toggleButton = document.querySelector('.switch');
 let body = document.body;
 let header = document.querySelector('header');
 let seachInput = document.querySelector('.search');
+let cardCircles = document.querySelectorAll('.card-circle');
 
 toggleButton.addEventListener('click', function() {
     toggleButton.classList.toggle('active');
     body.classList.toggle('dark-mode');
     header.classList.toggle('dark-mode');
     seachInput.classList.toggle('dark-mode');
+    cardCircles.forEach(function(cardCircle) {
+        cardCircle.classList.toggle('dark-mode');
+    });
 });
