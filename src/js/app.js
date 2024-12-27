9becc0b0ae62f1c43dbe3088046f6b4a6e1183fb
dhruv <dhruvrajput1904200@gmail.com>
2024-12-27 22:52:50 +0530

    Add dark mode support and enhance documentation styling

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,9 @@ let body = document.body;
 let header = document.querySelector('header');
 let seachInput = document.querySelector('.search');
 let cardCircles = document.querySelectorAll('.card-circle');
+let inlineCode = document.querySelectorAll('.inline-code');
+let codeCpp = document.querySelector(".code-cpp")
+
 
 toggleButton.addEventListener('click', function() {
     toggleButton.classList.toggle('active');
@@ -12,4 +15,8 @@ toggleButton.addEventListener('click', function() {
     cardCircles.forEach(function(cardCircle) {
         cardCircle.classList.toggle('dark-mode');
     });
+    inlineCode.forEach(function(inlineCode) {
+        inlineCode.classList.toggle('dark-mode');
+    });
+    codeCpp.classList.toggle('dark-mode');
 });
